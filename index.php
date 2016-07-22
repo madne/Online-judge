b30bab175934a9c3c27ea1639be0533d3823977a
john <johnnash957@gmail.com>
2016-07-22 12:24:12 -0700

    adding theme

--- index.php
+++ index.php
@@ -12,6 +12,41 @@
 	<div class="jumbotron">
 	   <h1 align="center">online judge</h1>
     </div>
-    <hr>
+
+
+    <div class="container">
+    	<div class="row">
+<div class="panel panel-default">
+  <div class="panel-body">
+
+    		    <div class="col-md-10"></div>
+    		    <div class="col-md-1">
+			      <button class="btn btn-default">Training</button>
+		    	</div>
+			    <div class="col-md-1">
+				  <button class="btn btn-default">Contests</button>
+				</div>
+				</div>
+</div>
+
+</div>
+</div>
+</div>
+<style>
+.footer{
+	position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    background-color: #F4511E;
+}
+.jumbotron{
+	background-color: #F4511E;
+}
+</style>
+<div class="footer">
+</div>
+
+
 </body>
 </html>
