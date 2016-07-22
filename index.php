c309a787f3921b8ffb4607af58b0892bff0eeb30
john <johnnash957@gmail.com>
2016-07-22 12:46:29 -0700

    adding theme

--- index.php
+++ index.php
@@ -7,46 +7,41 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href='css/style.css'>
+  <script src="js/main.js"></script>
 </head>
 <body>
 	<div class="jumbotron">
-	   <h1 align="center">online judge</h1>
+			<div class="col-md-11"></div>
+			<div class="col-md-1" style="position:fixed;top:6px;right:6px;	">
+				<button class="btn btn-default">Sign in </button>
+			</div>
+	   		<h1 align="center">online judge</h1>
     </div>
 
 
-    <div class="container">
-    	<div class="row">
-<div class="panel panel-default">
-  <div class="panel-body">
-
+<div class="container">
+    <div class="row">
+   		<div class="panel panel-default">
+  			<div class="panel-body">
     		    <div class="col-md-10"></div>
     		    <div class="col-md-1">
-			      <button class="btn btn-default">Training</button>
+			      	<button class="btn btn-default">Training</button>
 		    	</div>
 			    <div class="col-md-1">
 				  <button class="btn btn-default">Contests</button>
 				</div>
-				</div>
+			</div>
+		</div>
+    </div>
 </div>
+</div><!--container-->
 
-</div>
-</div>
-</div>
-<style>
-.footer{
-	position: fixed;
-    bottom: 0;
-    width: 100%;
-    height: 60px;
-    background-color: #F4511E;
-}
-.jumbotron{
-	background-color: #F4511E;
-}
-</style>
-<div class="footer">
-</div>
 
+
+
+<div class="footer"> 
+</div>
 
 </body>
 </html>
